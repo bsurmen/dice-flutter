5c9799287df893e5811c41e6c2b6d25fa5360d58
Burak Surmen <busurmen@gmail.com>
2019-12-16 19:46:09 +0300

    Create changing dice page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -12,11 +14,46 @@ void main() => runApp(MaterialApp(
       ),
     ));
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  _changeDiceFace();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  _changeDiceFace();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          )
+        ],
+      ),
     );
   }
+
+  void _changeDiceFace(){
+    setState(() {
+       leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 }
+
